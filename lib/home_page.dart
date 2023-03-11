4fc1d6e8cbc019dbc0f6458599e964b47c6c3c0e
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-03-11 23:54:00 +0100

    create individual_bar.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+/* 
+TASK!
+
+Input: List of weekly expenses = [sunAmount, monAmount, tueAmount, wedAmount, thuAmount, friAmount, satAmount]
+
+Output: Display input in a bar graph
+
+
+*/
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -8,6 +18,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<double> weeklySummary = [
+    20.88,
+    32.4,
+    19.3,
+    29.50,
+    50.99,
+    38.79,
+    78.30,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
