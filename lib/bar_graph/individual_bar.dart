e4336cb881151ad0b7c5de71099fbbccf08cf479
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-03-11 23:58:44 +0100

    update individual_bar

--- lib/bar_graph/individual_bar.dart
+++ lib/bar_graph/individual_bar.dart
@@ -1,1 +1,9 @@
-class IndividualBar {}
+class IndividualBar {
+  final int x; //position on x axis
+  final double y; //position on y axis
+
+  IndividualBar({
+    required this.x,
+    required this.y,
+  })
+}
