753c1e15cea5b76dcf93d439e0cd549c8f576fb5
Kingsley Asifor <kingsleyasifor@gmail.com>
2023-03-15 02:42:43 +0100

    initiate weeklySummary

--- lib/bar_graph/bar_graph.dart
+++ lib/bar_graph/bar_graph.dart
@@ -2,12 +2,16 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class MyBarGraph extends StatelessWidget {
-  const MyBarGraph({super.key});
+  final List weeklySummary; // [sunAmount, tueAmount ... satAmount]
+  const MyBarGraph({
+    super.key,
+    required this.weeklySummary,
+  });
 
   @override
   Widget build(BuildContext context) {
     return BarChart(BarChartData(
-      maxY: 100,
+      maxY: 200,
       minY: 0,
     ));
   }
